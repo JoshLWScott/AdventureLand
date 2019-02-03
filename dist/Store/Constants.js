247541d3e595e555afbc4b8baf85a82dec216420
Josh <joshscottmain@gmail.com>
2019-02-03 16:04:11 +0000

    Removing export's from Constants.ts

--- dist/Store/Constants.js
+++ dist/Store/Constants.js
@@ -1,9 +1,9 @@
-export var COMBAT_ENABLED = false;
-export var FOLLOW_TANK = false;
-export var FOCUS_TANK_TARGET = false;
+var COMBAT_ENABLED = false;
+var FOLLOW_TANK = false;
+var FOCUS_TANK_TARGET = false;
 /* Used to send stuff over to personal Merchant */
-export var MY_MERCHANT_NAME = "Scarlet";
-export var MY_PARTY_NAMES = [
+var MY_MERCHANT_NAME = "Scarlet";
+var MY_PARTY_NAMES = [
     "Besides",
     "Motivation",
     "Purity",
--- src/Store/Constants.ts
+++ src/Store/Constants.ts
@@ -1,13 +1,13 @@
 
-export const COMBAT_ENABLED: boolean = false;
-export const FOLLOW_TANK: boolean = false;
-export const FOCUS_TANK_TARGET: boolean = false
+const COMBAT_ENABLED: boolean = false;
+const FOLLOW_TANK: boolean = false;
+const FOCUS_TANK_TARGET: boolean = false
 
 
 /* Used to send stuff over to personal Merchant */
-export const MY_MERCHANT_NAME: string = "Scarlet";
+const MY_MERCHANT_NAME: string = "Scarlet";
 
-export const MY_PARTY_NAMES: Array<string> = [
+const MY_PARTY_NAMES: Array<string> = [
     "Besides", // Tank - Warrior
     "Motivation", // DPS - Mage
     "Purity", // Healer - Priest
